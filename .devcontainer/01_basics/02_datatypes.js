c50b2cc52213d47d566b0d2a5d2ecaa865dd67db
Salman13318 <salmansafdar03126810286@gmail.com>
2025-04-09 10:06:57 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all js code as newer version 
+
+// alert ( 3 + 3) // We are using node js, not browser
+
+// console.log( 3 
+    // + 
+    // 3)  // code readability should be high
+
+console.log("Salman");
+
+
+let Name= "Salman"
+let age= 23
+let isloggedin= false
+
+// number => 2 to power 53
+// BigInt
+// String =>""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+
+// Object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
+
